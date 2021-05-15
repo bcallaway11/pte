5301d5114709da558a2fbee05273278c9ccc4211
Brantly Callaway <brantly.callaway@uga.edu>
2021-05-14 23:53:34 -0400

    added attgt_fun

---
+++ R/covid_attgt.R
@@ -0,0 +1,93 @@
+     # get the correct "base" period for this group
+      # (subtract 2 to avoid anticipation)
+      main.base.period <- g - 1
+
+      #----------------------------------------------------
+      # if it's a pre-treatment time period (used for the
+      # pre-test, we need to adjust the base period)
+
+      # group not treated yet
+      if (tp < g) {
+        # move two periods before
+        base.period <- tp - 1
+      } else {
+        # this is a post-treatment period
+        base.period <- main.base.period
+      }
+      #----------------------------------------------------
+
+      #----------------------------------------------------
+      # now, all we need to do is collect the right subset
+      # of the data and estimate ATT(g,t)
+
+      # get group g and not-yet-treated group
+      this.data <- subset(data, G==g | G>tp | G==0)
+
+      # get current period and base period data
+      this.data <- subset(this.data, period==tp | period==base.period)
+      
+      # variable to keep track of pre/post periods
+      this.data$name <- ifelse(this.data$period==tp, "post", "pre")
+
+      # number of observations used for this (g,t)
+      n1 <- length(unique(this.data$id))
+      disidx <- unique(data$id) %in% unique(this.data$id)
+
+      # handle covariates
+      # for outcome regression, get pre-treatment values
+      Xor <- model.frame(or_xformla, data=subset(this.data,name=="pre"))
+      # for pscore, get pre-treatment values
+      Xpscore <- model.frame(pscore_xformla, data=subset(this.data,name=="pre"))
+
+      # convert two period panel into one period
+      this.data_outcomes <- tidyr::pivot_wider(this.data[,c("G","id","period","name","Y")], id_cols=c(id, G),
+                         names_from=c(name),
+                         values_from=c(Y))
+
+      # merge outcome and covariate data
+      this.dataOR <- cbind.data.frame(this.data_outcomes, Xor)
+      this.dataPscore <- cbind.data.frame(this.data_outcomes, Xpscore)
+
+      # create local treated variable
+      this.dataPscore$D <- 1*(this.dataPscore$G==g)
+
+      # run the outcome regression,
+      # note: this is regression of change on covariates (which makes sense given structure of model in paper)
+      or_formla <- BMisc::toformula("I(post-pre)", BMisc::rhs.vars(or_xformla))
+      outcome_regression <- lm(or_formla,data=subset(this.dataOR, (G > g | G == 0)))
+      # and get predicted values
+      # note: add back in pre-treatment outcomes so that this
+      # is prediction for the level of outcomes in post-treatment
+      # period
+      or_preds <- predict(outcome_regression, newdata=this.dataOR) + this.dataOR$pre
+
+      # run the pscore regression
+      pscore_formla <- BMisc::toformula("D", c(BMisc::rhs.vars(pscore_xformla)))
+
+      # warn about units violating common support
+      # keepers provides a way to drop problematic pscores,
+      # but current version just warns
+      keepers <- rep(TRUE, nrow(this.dataPscore))
+      #if (!boot_iter) {
+      ##  keepers <- pscore_preds < .99
+      ##   droppers <- pscore_preds > .99
+      ##   if (sum(droppers) > 0) {
+      ##     warn_message <- paste0(sum(droppers), " observations have pscores greater than .99 with ids: ", paste0(this.dataPscore[droppers==1,]$id, collapse=", "), ". These likely indicate violations of the overlap condition and you should consider dropping these observations.")
+      ##     warning(warn_message)
+      ##   }
+      ## }
+
+      # treatment dummy variable
+      D <- this.dataPscore$D[keepers]
+      #pscore_preds <- pscore_preds[keepers]
+
+      Y_post <- this.dataOR$post[keepers]
+
+ 
+      this.dataPscore <- droplevels(this.dataPscore)
+      attgt <- DRDID::drdid_panel(y1=Y_post,
+                                        y0=rep(0,length(Y_post)),
+                                        D=D,
+                                        covariates=model.matrix(pscore_formla,
+                                                                data=this.dataPscore),
+                                        inffunc=TRUE)
--- R/pte.R
+++ R/pte.R
@@ -1,11 +1,110 @@
 compute.pte <- function(ptep,
                         attgt_fun,
                         ...) {
+  #-----------------------------------------------------------------------------
+  # unpack ptep
+  #-----------------------------------------------------------------------------
+  data <- ptep$data
+  yname <- ptep$yname
+  gname <- ptep$gname
+  idname <- ptep$idname
+  tname <- ptep$tname
+  
+  
+  data <- as.data.frame(data)
+  
+  # setup data
+  G <- data[,gname]
+  id <- data[,idname]
+  period <- data[,tname]
+
+  data$G <- G
+  data$id <- id
+  n <- length(unique(data$id))
+  data$period <- period
+  data$Y <- data[,yname]
+
+  # pick up all time periods
+  time.periods <- ptep$tlist
+
+  # sort the groups and drop the untreated group
+  groups <- ptep$glist
+
+
+  # list to store all group-time average treatment effects
+  # that we calculate
+  attgt.list <- list()
+  counter <- 1
+  nG <- length(groups)
+  nT <- length(time.periods)
+  inffunc <- matrix(data=0, nrow=n, ncol=nG*(nT))
+  
+  # loop over all groups
+  for (g in groups) {
+        
+    # loop over all time periods
+    for (tp in time.periods) {
+
+      attgt <- attgt_fun(...)
+
+      #-----------------------------------------------------------------------------
+      # If this is too generic...
+      # an alternative idea is to just delete the call to attgt_fun above
+      # and just write your own code in this spot
+      #-----------------------------------------------------------------------------
+
+ 
+      attgt$att.inf.func <- (n/n1)*attgt$att.inf.func
+      # save results
+      attgt.list[[counter]] <- list(att=attgt$ATT, group=g, time.period=tp)
+
+      this.inf_func <- rep(0,n)
+      this.inf_func[disidx] <- attgt$att.inf.func
+      inffunc[,counter] <- this.inf_func
+
+
+      #cat("counter: ", counter, "\n")
+      counter <- counter+1
+      #----------------------------------------------------
+
+    }
+  }
+
+  return(list(attgt.list=attgt.list, inffunc=inffunc))
 
 }
 
 
-
+#' @title pte
+#'
+#' @description Main function for computing panel treatment effects
+#'
+#' @inheritParams pte_params
+#' @param attgt_fun This is a function that should work in the case where
+#'  there is a single group and the "right" number of time periods to
+#'  recover an estimate of the ATT.  For example, in the contest of
+#'  difference in differences, it would need to work for a single group,
+#'  find the appropriate comparison group (untreated units), find the right
+#'  time periods (pre- and post-treatment), and then recover an estimate
+#'  of ATT for that group.  It will be called over and over separately
+#'  by groups and by time periods to compute ATT(g,t)'s.
+#'
+#'  The function needs to work in a very specific way.  It should take in the
+#'  arguments: \code{data}, \code{group}, \code{time.periods}, \code{glist},
+#'  \code{tlist}, and \code{...}.  \code{...} are additional arguments (such as
+#'  formulas for covariates) that \code{attgt_fun} needs.  From these arguments
+#'  \code{attgt_fun} must return a list with element \code{ATT} containing the
+#'  group-time average treatment effect for that group and that time period.
+#'
+#'  If \code{attgt_fun} returns an influence function (which should be provided
+#'  in a list element named \code{inf_func}), then the code will use the
+#'  multiplier bootstrap to compute standard errors for group-time average
+#'  treatment effects, an overall treatment effect parameter, and a dynamic
+#'  treatment effect parameter (i.e., event study parameter).  If
+#'  \code{attgt_fun} does not return an influence function, then the same
+#'  objects will be computed using the empirical bootstrap.  This is usually
+#'  (perhaps substantially) easier to code, but also will usually be (perhaps
+#'  substantially) computationally slower.
 pte <- function(yname,
                 gname,
                 tname,
@@ -30,10 +129,8 @@ pte <- function(yname,
   res <- compute.pte(ptep=ptep,
                      attgt_fun=attgt_fun,
                      ...)
-               
   
-  
-  att_gt <- process_att_gt(res,dp)
+  att_gt <- process_att_gt(res,ptep)
   
   return(att_gt)
 
