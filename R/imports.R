a2df3e30ea443d9f42c84d64e83208595f6d0bf7
Brantly Callaway <brantly.callaway@uga.edu>
2025-01-07 09:22:37 -0500

    update package documentation

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,6 @@
-#' @title Panel Treatment Effects
-#' @description Tools for estimating treatment effects with panel data
+#' Panel Treatment Effects
+#'
+#' Tools for estimating treatment effects with panel data.
 #'
 #' @docType package
 #' @name pte
@@ -7,5 +8,6 @@
 #' @import utils
 #' @import ggplot2
 #' @import tidyr
+#' @import BMisc
 #' @importFrom dplyr bind_rows group_by group_map inner_join mutate transmute
-NULL
+"_PACKAGE"
